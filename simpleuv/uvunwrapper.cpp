b714fbefd2413668149b409fa9fc17b735f6ab73
ZerongZheng <zrzheng1995@foxmail.com>
2019-12-09 15:45:09 +0800

    bug fix

--- simpleuv/uvunwrapper.cpp
+++ simpleuv/uvunwrapper.cpp
@@ -415,7 +415,7 @@ void UvUnwrapper::calculateSizeAndRemoveInvalidCharts()
     auto charts = m_charts;
     auto chartSourcePartitions = m_chartSourcePartitions;
     m_charts.clear();
-    chartSourcePartitions.clear();
+    m_chartSourcePartitions.clear();
     for (size_t chartIndex = 0; chartIndex < charts.size(); ++chartIndex)
     {
         auto &chart = charts[chartIndex];
